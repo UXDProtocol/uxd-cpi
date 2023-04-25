e010ecbc34148282543c843a3f7b6b485003c397
Vincent Brunet <vincent.brunet.us@gmail.com>
2023-04-25 17:08:28 +0900

    using-the-correct-uxd-program-ids

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
 anchor_gen::generate_cpi_crate!("idl.json");
 
-declare_id!("Govz1VyoyLD5BL6CSCxUJLVLsQHRwjfFj1prNsdNg5Jw");
+#[cfg(feature = "development")]
+declare_id!("8PEASVXk2zt1oDRUgUFCZeV7QjaYTxzYwn5ZoZWwKDoC");
+#[cfg(not(feature = "development"))]
+declare_id!("UXD8m9cvwk4RcSxnX2HZ9VudQCEeDH6fRnB4CAP57Dr");
