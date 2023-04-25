e83ed8b8f74563035e014e1c9e8d89bb5006e42d
Vincent Brunet <vincent.brunet.us@gmail.com>
2023-04-25 17:59:52 +0900

    program-id-for-devnet

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 anchor_gen::generate_cpi_crate!("idl.json");
 
 #[cfg(feature = "development")]
-declare_id!("8PEASVXk2zt1oDRUgUFCZeV7QjaYTxzYwn5ZoZWwKDoC");
+declare_id!("EqMrasrCXsbE6mpXaopUH1qip7auwX4AF5UWWpXaLr6u");
 #[cfg(not(feature = "development"))]
 declare_id!("UXD8m9cvwk4RcSxnX2HZ9VudQCEeDH6fRnB4CAP57Dr");
