d2eb7c9d573a1a988a4b24a6b1ec05ee94506402
Vincent Brunet <vincent.brunet.us@gmail.com>
2023-08-25 21:15:05 +0900

    update-program-id

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 anchor_gen::generate_cpi_crate!("idl.json");
 
 #[cfg(feature = "development")]
-declare_id!("EqMrasrCXsbE6mpXaopUH1qip7auwX4AF5UWWpXaLr6u");
+declare_id!("CW5VzSk7WC4NPyuNt19VFev9FUHhyk5xxHTj2DUWBexu");
 #[cfg(not(feature = "development"))]
 declare_id!("UXD8m9cvwk4RcSxnX2HZ9VudQCEeDH6fRnB4CAP57Dr");
